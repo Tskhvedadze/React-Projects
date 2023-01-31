0a0059ff8b77385ef6ec66bda54eebdfa6847791
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-01-31 22:20:14 +0400

    added tours projects files and modified it

---
+++ tours/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './components/Loading'
+import Tours from './components/Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+    const [loading, setLoading] = useState(true)
+    const [tours, setTours] = useState([])
+
+    const fethTours = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(url)
+            const tours = await response.json()
+            setLoading(false)
+            setTours(tours)
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fethTours()
+    }, [])
+
+    if (loading) {
+        return (
+            <main>
+                <Loading />
+            </main>
+        )
+    }
+
+    return (
+        <main>
+            <Tours tours={tours} />
+        </main>
+    )
+}
+
+export default App
