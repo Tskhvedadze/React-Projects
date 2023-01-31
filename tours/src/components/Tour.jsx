01cfd8b64cec2dec8c7112f008d4bf7d10045b46
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-01-31 22:41:13 +0400

    added toggle buttons logic

--- tours/src/components/Tour.jsx
+++ tours/src/components/Tour.jsx
@@ -11,7 +11,14 @@ const Tour = ({ id, image, info, name, price }) => {
                     <h4>{name}</h4>
                     <h4 className='tour-price'>${price}</h4>
                 </div>
-                <p>{info}</p>
+                <p>
+                    {readMore ? info : `${info.substring(0, 200)}...`}
+                    <button
+                        onClick={() => setReadMore((prevState) => !prevState)}
+                    >
+                        {(!readMore && 'read more') || 'show less'}
+                    </button>
+                </p>
                 <button className='delete-btn'>not interested</button>
             </footer>
         </article>
