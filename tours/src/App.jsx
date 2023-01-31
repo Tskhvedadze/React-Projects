c8cc211d340c3378c5035f16f1d86cf66512dd47
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-01-31 22:55:19 +0400

    added remove tours logic and complet this project

--- tours/src/App.jsx
+++ tours/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
     const [loading, setLoading] = useState(true)
     const [tours, setTours] = useState([])
 
+    const removeTour = (id) => {
+        const newTours = tours.filter((tour) => tour.id !== id)
+        setTours(newTours)
+    }
+
     const fethTours = async () => {
         setLoading(true)
         try {
@@ -33,9 +38,22 @@ function App() {
         )
     }
 
+    if (!tours.length) {
+        return (
+            <main>
+                <div className='title'>
+                    <h2>no tours left</h2>
+                    <button className='btn' onClick={fethTours}>
+                        refresh
+                    </button>
+                </div>
+            </main>
+        )
+    }
+
     return (
         <main>
-            <Tours tours={tours} />
+            <Tours tours={tours} removeTour={removeTour} />
         </main>
     )
 }
--- tours/src/components/Tour.jsx
+++ tours/src/components/Tour.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Tour = ({ id, image, info, name, price }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
     const [readMore, setReadMore] = useState(false)
 
     return (
@@ -19,7 +19,9 @@ const Tour = ({ id, image, info, name, price }) => {
                         {(!readMore && 'read more') || 'show less'}
                     </button>
                 </p>
-                <button className='delete-btn'>not interested</button>
+                <button className='delete-btn' onClick={() => removeTour(id)}>
+                    not interested
+                </button>
             </footer>
         </article>
     )
