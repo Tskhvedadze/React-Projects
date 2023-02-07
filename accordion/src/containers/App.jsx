f0a99dd78b4736828c84a384eb5e9a21793db9d3
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-02-07 22:12:08 +0400

    completed accordings project

--- accordion/src/containers/App.jsx
+++ accordion/src/containers/App.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { data } from '../data/data'
 import SingleQuestion from '../views/Question'
 function App() {
-    return <h2>accordion project setup</h2>
+    return (
+        <main>
+            <div className='container'>
+                <h3>questions and answers about login</h3>
+                <section className='info'>
+                    {data.map((question) => {
+                        return (
+                            <SingleQuestion key={question.id} {...question} />
+                        )
+                    })}
+                </section>
+            </div>
+        </main>
+    )
 }
 
 export default App
