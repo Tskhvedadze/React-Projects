a83ad714bbd9b801b7b3e9e51b6384340e440e6f
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-02-11 23:48:52 +0400

    added slideres style and logic

--- slider/src/container/App.jsx
+++ slider/src/container/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 
 import data from '../data/data'
-import Btn from '../components/Btn'
+
 function App() {
     const [people, setPeople] = useState(data)
     const [index, setIndex] = useState(0)
@@ -16,29 +17,41 @@ function App() {
                 </h2>
             </div>
             <div className='section-center'>
-                {people.map(({ id, image, name, quote, title }, index) => {
-                    // more stuff coming up
+                {people.map(
+                    ({ id, image, name, quote, title }, personIndex) => {
+                        // more stuff coming up
+                        let position = 'nextSlide'
+                        if (personIndex === index) {
+                            position = 'activeSlide'
+                        }
+                        if (
+                            personIndex === index - 1 ||
+                            (index === 0 && personIndex === people.length - 1)
+                        ) {
+                            position = 'lastSlide'
+                        }
 
-                    return (
-                        <article key={id}>
-                            <img
-                                src={image}
-                                alt={name}
-                                className='person-img'
-                            />
-                            <h4>{name}</h4>
-                            <p className='title'>{title}</p>
-                            <p className='text'>{quote}</p>
-                            <FaQuoteRight className='icon' />
-                        </article>
-                    )
-                })}
-                <Btn className='prev'>
+                        return (
+                            <article className={position} key={id}>
+                                <img
+                                    src={image}
+                                    alt={name}
+                                    className='person-img'
+                                />
+                                <h4>{name}</h4>
+                                <p className='title'>{title}</p>
+                                <p className='text'>{quote}</p>
+                                <FaQuoteRight className='icon' />
+                            </article>
+                        )
+                    },
+                )}
+                <button className='prev'>
                     <FiChevronLeft />
-                </Btn>
-                <Btn className='next'>
+                </button>
+                <button className='next'>
                     <FiChevronRight />
-                </Btn>
+                </button>
             </div>
         </section>
     )
