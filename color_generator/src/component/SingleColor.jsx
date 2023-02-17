7ff7c697bf8f9d459cd0bd8b63257d903f9b481d
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-02-17 23:48:25 +0400

    completed the cliapboards logic

--- color_generator/src/component/SingleColor.jsx
+++ color_generator/src/component/SingleColor.jsx
@@ -8,13 +8,28 @@ const SingleColor = ({ rgb, weight, index }) => {
     const bcg = rgb.join(',')
     const hex = rgbToHex(...rgb)
 
+    const handleClick = () => {
+        setAlert(true)
+        navigator.clipboard.writeText(hex)
+    }
+
+    useEffect(() => {
+        const timeOut = setTimeout(() => {
+            setAlert(false)
+        }, 2000)
+
+        return () => clearTimeout(timeOut)
+    }, [alert])
+
     return (
         <article
             className={`color ${index > 10 && 'color-light'}`}
             style={{ backgroundColor: `rgb(${bcg})` }}
+            onClick={handleClick}
         >
-            <p className='pecent-value'>{weight}%</p>
+            <p className='percent-value'>{weight}%</p>
             <p className='color-value'>{hex}</p>
+            {alert && <p className='alert'>copied to clipboard</p>}
         </article>
     )
 }
--- color_generator/src/container/App.jsx
+++ color_generator/src/container/App.jsx
@@ -6,16 +6,18 @@ import SingleColor from '../component/SingleColor'
 function App() {
     const [color, setColor] = useState('')
     const [error, setError] = useState(false)
-    const [list, setList] = useState([])
+    const [list, setList] = useState(new Values('#f15025').all(10))
 
     const handleSubmit = (e) => {
         e.preventDefault()
         try {
             let colors = new Values(color).all(10)
+            setError(false)
             setList(colors)
         } catch (error) {
             setError(true)
-            console.log(error)
+            setList([])
+            console.error(error)
         }
     }
     return (
